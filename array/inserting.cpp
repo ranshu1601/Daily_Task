5e0a37d648e8145a305d734e05562e187c2878c1
Anshu Ranjan <lowkeyreal247@gmail.com>
2023-08-04 14:39:46 +0530

    insertion

--- array/inserting.cpp
+++ array/inserting.cpp
@@ -43,29 +43,57 @@
 //     displayArr(arr, n);
 // }
 
+// #include <iostream>
+// using namespace std ; 
+// class Array{
+//     public:
+//         int A[10];
+//         int size ; 
+//         int length ; 
+// };
+
+// void Display(Array arr){
+//     int i ; 
+//     cout<<"arr.length: "<<arr.length<<endl ; 
+//     for(i=0 ; i<arr.length ; i++){
+//         cout<<arr.A[i]<<" ";
+//     }
+// }
+
+// int main()
+// {
+// Array arr1={{2,3,4,5,6},10,5};
+// // Append(&arr1,10);
+// // Insert(&arr1,0,12);
+// Display(arr1);
+//  return 0;
+// }
+
+
 #include <iostream>
-using namespace std ; 
-class Array{
+using namespace std  ;
+
+class Array {
     public:
         int A[10];
-        int size ; 
         int length ; 
+        int size ;
+
 };
 
-void Display(Array arr){
-    int i ; 
-    cout<<"arr.length: "<<arr.length<<endl ; 
-    for(i=0 ; i<arr.length ; i++){
+void displayArr(Array arr , Array length){
+    for(int i=0 ; i<arr.length ; i++){
         cout<<arr.A[i]<<" ";
     }
 }
 
-int main()
-{
-Array arr1={{2,3,4,5,6},10,5};
-// Append(&arr1,10);
-// Insert(&arr1,0,12);
-Display(arr1);
- return 0;
-}
+void appendArr(Array *arr , int value){
+    if(arr->length<arr->size){
+        arr->A[arr->length++] = value;
+    }
 
+}
+int main(){
+
+
+}
