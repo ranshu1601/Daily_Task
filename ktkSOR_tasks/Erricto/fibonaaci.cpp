db9a27bda0f76311d2671a7675bab88835672121
Anshu Ranjan <lowkeyreal247@gmail.com>
2023-08-06 15:05:20 +0530

    fibo

---
+++ ktkSOR_tasks/Erricto/fibonaaci.cpp
@@ -0,0 +1,41 @@
+// Fibonaaci with Iteration
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   int num, t1 = 0, t2 = 1, nextNum;
+//   cout << "Enter Number: ";
+//   cin >> num;
+//   for (int i = 1; i <= num; i++) {
+//     if (i == 1) {
+//       cout << t1 << " ";
+//       continue;
+//     } else if (i == 2) {
+//       cout << t2 << " ";
+//       continue;
+//     }
+//     nextNum = t1 + t2;
+//     t1 = t2;
+//     t2 = nextNum;
+//     cout << nextNum << " ";
+//   }
+// }
+
+// Fibonaaci with recusrion nth fibonaaci number 
+
+// #include <iostream>
+// using namespace std ; 
+
+// int fibo(int num){
+//     if(num<=1){
+//         return num;
+//     }
+//     else{
+//         return fibo(num-1)+fibo(num-2);
+//     }
+// }
+
+// int main(){
+//     cout<<fibo(10);
+// }
