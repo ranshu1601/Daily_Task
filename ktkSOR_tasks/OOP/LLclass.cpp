dd349b67f581c91553eeebf155d485817c90b6ef
Anshu Ranjan <ranshu1601@gmail.com>
2023-08-11 18:34:53 +0530

    git

--- ktkSOR_tasks/OOP/LLclass.cpp
+++ ktkSOR_tasks/OOP/LLclass.cpp
@@ -10,6 +10,7 @@ public:
 class anshuNode : public Node {
   int length;
   Node *head = new Node;
+  Node *ptr = head;
   Node *last;
 
 public:
@@ -19,9 +20,10 @@ public:
   int min();
   int max();
   int countNode();
-  void pushBack();
-  void pushFront();
-  void insertAt();
+  bool isPresent(int key);
+  void insertAfter(int index, int value);
+  void insertHead(int value);
+  void popAt(int index);
   void sortLL();
 };
 
@@ -48,10 +50,13 @@ void anshuNode::createLL(int arr[], int len) {
 void anshuNode::displayLL() {
   Node *ptr = head;
   while (ptr != nullptr) {
-    cout << ptr->data << "->";
+    cout << ptr->data <<" ";
     ptr = ptr->next;
   }
 }
+
+// 3. REVERSE
+void anshuNode::reverseLL() {}
 
 // 4. MIN
 int anshuNode::min() {
@@ -90,9 +95,81 @@ int anshuNode::countNode() {
   return count;
 }
 
+// 7. IS PRESENT
+bool anshuNode::isPresent(int key) {
+  Node *ptr = head;
+  bool flag = true;
+  while (ptr != nullptr) {
+    if (ptr->data == key) {
+      flag = flag;
+      break;
+    }
+    else{
+      flag=!flag;
+    }
+    ptr = ptr->next;
+  }
+  return flag;
+}
+
+// 8. INSERT-AFTER [INDEX]
+void anshuNode::insertAfter(int index, int value) {
+  Node *temp = new Node;
+  temp->data = value;
+  for (int i = 0; i < index - 1; i++) {
+    ptr = ptr->next;
+  }
+  temp->next = ptr->next;
+  ptr->next = temp;
+}
+
+// 9. INSERT-HEAD
+void anshuNode::insertHead(int value) {
+  Node *temp = new Node;
+  temp->data = value;
+  temp->next = head;
+  head = temp;
+}
+
+// 10. POP
+void anshuNode::popAt(int index){
+  if(index==1){
+    Node *temp = ptr;
+    ptr=ptr->next;
+    delete temp;
+  }
+  else{
+    Node *p = head;
+    Node *q = nullptr;
+
+    for(int i=0 ; i<index-1 ; i++){
+      q=p;
+      p=p->next;
+    }
+    q->next = p->next;
+    delete p;
+  }
+}
+
+// 11. SORT
+void anshuNode::sortLL() {}
+
 int main() {
   int arr[] = {1, 2, 3, 4, 5};
   anshuNode ktk;
   ktk.createLL(arr, 5);
+  ktk.displayLL();
+  cout<<"\n"<<ktk.min();
+  cout<<"\n"<<ktk.max();
+  cout<<"\n"<<ktk.countNode();
+  cout<<"\n"<<ktk.isPresent(1);
+  cout<<"\n";
+  ktk.insertAfter(4, 9);
+  ktk.displayLL();
+  cout<<"\n";
+  ktk.insertHead(7);
+  ktk.displayLL();
+  cout<<"\n";
+  ktk.popAt(3);
   ktk.displayLL();
 }
