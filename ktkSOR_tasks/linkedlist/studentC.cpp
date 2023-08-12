a58b7fd96eee02da5723eb483e9c43cdc63e72e8
Anshu Ranjan <ranshu1601@gmail.com>
2023-08-13 00:31:09 +0530

    SC1

--- ktkSOR_tasks/linkedlist/studentC.cpp
+++ ktkSOR_tasks/linkedlist/studentC.cpp
@@ -1,63 +1,62 @@
 // Finding Middle element Of LL
 
-#include<iostream>
-using namespace std ; 
+#include <iostream>
+using namespace std;
 
-class Node{
-    public:
-        int data;
-        Node *next;
+class Node {
+public:
+  int data;
+  Node *next;
 };
 
 class doNode : public Node {
-    Node *head = new Node;
-    Node *last;
+  Node *head = new Node;
+  Node *last;
 
-    // Functions
-    public:
-        void create(int arr[] , int len);
-        void middle(int len);
+  // Functions
+public:
+  void create(int arr[], int len);
+  void middle(int len);
 };
 
-void doNode::create(int arr[], int len){
-    head->data = arr[0];
-    head->next = nullptr ; 
+void doNode::create(int arr[], int len) {
+  head->data = arr[0];
+  head->next = nullptr;
 
-    last = head;
+  last = head;
 
-    for(int i=1 ; i<len ; i++){
-        Node *temp = new Node ; 
-        temp->data = arr[i];
-        temp->next = nullptr;
+  for (int i = 1; i < len; i++) {
+    Node *temp = new Node;
+    temp->data = arr[i];
+    temp->next = nullptr;
 
-        last->next = temp;
-        last = temp;
-    }
+    last->next = temp;
+    last = temp;
+  }
 
-    Node *ptr = head;
-    while(ptr!=nullptr){
-        cout<<ptr->data<<" ";
-        ptr = ptr->next;
-    }
-
+  Node *ptr = head;
+  while (ptr != nullptr) {
+    cout << ptr->data << " ";
+    ptr = ptr->next;
+  }
 }
 
-void doNode::middle(int len){
-    int mid = len/2;
-    int i=1;
-    Node *p = head;
-    while(i<=mid){
-        p=p->next;
-        i++;
-    }
-    cout<<"Num: "<<p->data;
+void doNode::middle(int len) {
+  int mid = len / 2;
+  int i = 1;
+  Node *p = head;
+  while (i <= mid) {
+    p = p->next;
+    i++;
+  }
+  cout << "Num: " << p->data;
 }
 
-int main(){
-    doNode ktk;
-    int arr[] ={3,5,6,7,9,10};
-    int n = sizeof(arr)/sizeof(int); 
-    ktk.create(arr, n);
-    cout<<"\n";
-    ktk.middle(n);
+int main() {
+  doNode ktk;
+  int arr[] = {3, 5, 6, 7, 9, 10};
+  int n = sizeof(arr) / sizeof(int);
+  ktk.create(arr, n);
+  cout << "\n";
+  ktk.middle(n);
 }
