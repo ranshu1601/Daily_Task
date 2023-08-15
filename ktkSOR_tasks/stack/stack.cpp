b58e1b8c8ce93b0246d13ca999ae58ea0176b844
Anshu Ranjan <ranshu1601@gmail.com>
2023-08-15 13:31:48 +0530

    stack

---
+++ ktkSOR_tasks/stack/stack.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std ; 
+
+class stack{
+    public:
+        int top;
+        int size;
+        int *S;
+};
+
+int main(){
+    stack ktk;
+    cout<<"Enter Size: ";
+    cin>>ktk.size;
+
+}
