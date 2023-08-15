7aab01f3ad8e8838ffcfd097d99155b13dc343c7
Anshu Ranjan <ranshu1601@gmail.com>
2023-08-16 00:35:33 +0530

    idk

---
+++ accolite/shaddhaDIDI/maxSubarray.cpp
@@ -0,0 +1,40 @@
+/* Given an integer array nums, find the subarray with the largest sum, and
+ * return its sum. */
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std ; 
+
+int main(){
+    vector<int> arr;
+    int length ; 
+    cout<<"Length: ";
+    cin>>length;
+
+    for(int i=0 ; i<length ; i++){
+        int value ; 
+        cout<<"enter: ";
+        cin>>value;
+        arr.push_back(value);
+    }
+
+    int maxsum = INT_MIN;
+    for(int i=0 ; i<length ; i++){
+        int sum ;
+        for(int j=0 ; j<=i ; j++){
+            sum = sum+arr[j];
+            maxsum = max(sum,maxsum);
+        }
+        
+    }
+
+    cout<<maxsum;
+}
+
+
+//  Kadane's algorithm 
+
+
+
