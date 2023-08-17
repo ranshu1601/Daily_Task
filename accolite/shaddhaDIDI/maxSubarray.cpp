22ffa63141533fc9892dfd58baeab91979deb338
Anshu Ranjan <ranshu1601@gmail.com>
2023-08-17 23:33:48 +0530

    mode

--- accolite/shaddhaDIDI/maxSubarray.cpp
+++ accolite/shaddhaDIDI/maxSubarray.cpp
@@ -32,6 +32,7 @@ int main(){
 
     cout<<maxsum;
 }
+// [-2,1,-3,4,-1,2,1,-5,4]
 
 
 //  Kadane's algorithm 
---
+++ mode.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+    int size;
+    cout<<"size: ";
+    cin>>size;
+    int *arr = new int[size];
+}
