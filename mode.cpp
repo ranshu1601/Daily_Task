fa537ec8d06f17f5b378c88e99871b8f6bdf16cb
Anshu Ranjan <ranshu1601@gmail.com>
2023-08-17 23:48:05 +0530

    mode

--- mode.cpp
+++ mode.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
-using namespace std ;
+using namespace std;
 
-int main(){
-    int size;
-    cout<<"size: ";
-    cin>>size;
-    int *arr = new int[size];
+void mode(int arr[], int size) {
+  int count = 0;
+  for (int i = 0; i < size; i++) {
+    for (int j = i + 1; j < size; j++) {
+      if (arr[i] == arr[j])
+        count++;
+    }
+  }
+  cout << count;
+}
+
+int main() {
+  int size;
+  cout << "size: ";
+  cin >> size;
+  int *arr = new int[size];
+
+  for (int i = 0; i < size; i++) {
+    cout << "enter: ";
+    cin >> arr[i];
+  }
+
+  mode(arr, size);
 }
