ecc0f8d295ad5377996998e98366db5056ee3978
Anshu Ranjan <ranshu1601@gmail.com>
2023-08-25 18:04:02 +0530

    hcf-Recursion

---
+++ ktkSOR_tasks/hcfRecusrion.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int hcf(int num1, int num2) {
+  if (num1 == 0 || num2 == 0) {
+    return 0;
+  } else if (num1 == num2) {
+    return num1;
+  } else if (num1 > num2) {
+    return hcf(num1 % num2, num2);
+  } else {
+    return hcf(num1, num2 % num1);
+  }
+}
+
+int main() {
+  int num1, num2;
+  cout << "Enter num1: ";
+  cin >> num1;
+  cout << "Enter num2: ";
+  cin >> num2;
+  cout<<hcf(num1,num2);
+}
